715d3ddebc01d9e03c43d22affe015dce9f18997
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-02-21 14:08:41 +0000

    chore: fix linter issues

--- docs/src/examples/media/HomePage.tsx
+++ docs/src/examples/media/HomePage.tsx
@@ -10,7 +10,7 @@ export function HomePage() {
       </div>
 
       <div className="max-w-5xl md:mx-3 lg:m-auto">
-        <div className="divide-y divide-gray-200 rounded border-t border-b border-gray-200 dark:divide-gray-900 dark:border-gray-900 md:border">
+        <div className="divide-y divide-gray-200 rounded border-b border-t border-gray-200 md:border dark:divide-gray-900 dark:border-gray-900">
           <ArticlePreview mainImage="https://images.unsplash.com/photo-1636730987934-c6a10afd64d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80" />
           <ArticlePreview mainImage="https://images.unsplash.com/photo-1545599902-e86e23188f72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2728&q=80" />
           <ArticlePreview mainImage="https://images.unsplash.com/photo-1511131341194-24e2eeeebb09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" />
